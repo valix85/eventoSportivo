4e36bf3e9c5996e04daa7ff8f384b8c93a03b3f9
valix85 <valix85@gmail.com>
2017-02-10 10:38:37 +0100

    test sync gut

--- src/org/valix85/classifiche/Tempi.java
+++ src/org/valix85/classifiche/Tempi.java
@@ -2,26 +2,23 @@ package org.valix85.classifiche;
 
 import org.valix85.anagrafica.Persona;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 /**
  * Created by Valerio on 09/02/2017.
  */
-public class Tempi<T extends Persona> {
+public class Tempi {
 
-    private Map<Integer, T> tab = new TreeMap<>();
 
-    public void add(T p, int sec){
-        tab.put(sec, p);
+
+    public void add(Persona p, int sec){
+        tab.put(p,sec);
     }
 
-    /**
-     * Impedisco che la classifica venga modificata da fuori
-     * @return
-     */
-    public Map<Integer, T> getClassifica() {
-        return Collections.unmodifiableMap(tab);
+
+    public void ordina() {
+        //TODO...
+        //...test git
     }
 }
